9b09378590a0b4d4a4d098b0fa92e5d51fb5ea34
engr-Godswill <ochigodswill@gmail.com>
2023-02-20 08:22:31 -0800

    gf

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'z';
+	while
+		(i >= 'a') {
+			putchar(i);
+			i--;
+		}
+	putchar('\n');
+	return (0);
+}
