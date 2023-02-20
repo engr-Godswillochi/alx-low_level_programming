9069937ff2acd4fa40ce5f17f859432bfa892859
engr-Godswill <ochigodswill@gmail.com>
2023-02-20 08:14:42 -0800

    fg

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets in lower case except q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
