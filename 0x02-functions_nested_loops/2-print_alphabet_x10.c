b18f5483270ea3ac7fd1e2e4840b40969ecde304
engr-Godswill <ochigodswill@gmail.com>
2023-02-22 13:08:47 -0800

    first trial for 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,14 @@
   */
 void print_alphabet_x10(void)
 {
-  char alphabets[26] = "abcdefghijlomnopqrstuvwxyz";
-  int a = 0;
-  int i;
-  while(a < 10){
+char alphabets[26] = "abcdefghijlomnopqrstuvwxyz";
+int a = 0;
+int i;
+ while (a < 10)
+  {
     for (i = 0; i < 26; i++)
       {
-	putchar(alphabets[i]);
+	putchar (alphabets[i]);
       }
     putchar('\n');
     a++;
