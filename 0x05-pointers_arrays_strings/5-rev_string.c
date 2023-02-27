7c83ef61405be14667dd1aa25ee90a32446a10f3
engr-Godswill <ochigodswill@gmail.com>
2023-02-27 13:50:57 -0800

    maing some corrections on 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,32 +1,35 @@
 #include "main.h"
+#include "2-strlen.c"
+#include <stdlib.h>
+
 /**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
+ * rev_string - reverses a string
+ * @s: string to reverse
+ * Return: void
+ */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	int len = _strlen(s);
+	char *d_s = NULL, *dummy = s, *holder = NULL, *s_holder = s;
 
-	a = s;
-
-	while (s[c] != '\0')
+	if (len == 0)
+		goto end;
+	d_s = (char *) malloc(len + 1);
+	holder = d_s;
+	while ((*dummy) != '\0')
 	{
-		c++;
+		(*d_s) = (*dummy);
+		dummy++;
+		d_s++;
 	}
 
-	for (k = 1; k < c; k++)
+	d_s--; /* to remove the unknown memory value */
+	while (d_s != holder)
 	{
-		a++;
+		(*s) = (*d_s);
+		d_s--, s++;
 	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+	d_s = holder, (*s) = (*d_s);
+	(*(++s)) = '\0'; /* string terminator */
+end:	s = s_holder;
 }
