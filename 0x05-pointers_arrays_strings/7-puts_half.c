e533549f521780a03de36e09d098da51fc7908ed
engr-Godswill <ochigodswill@gmail.com>
2023-02-27 14:43:16 -0800

    updated 7-puts_half to print half part of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *puts_half - print the last half of a string
+ *@str: the string to be worked on
+ *
+ *return - voifd
+ */
+
+void puts_half(char *str)
+{
+int i, n;
+int j = 0;
+
+while (str[j] != '\0')
+{
+j++;
+}
+if (j % 2 == 0)
+{
+n = j / 2;
+}
+else
+{
+n = (j - 1) / 2;
+}
+for (i = j; i < n; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
