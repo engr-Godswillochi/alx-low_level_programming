495358be4aa51a9dc04fae5ecfd047a1d3cb7981
engr-Godswill <ochigodswill@gmail.com>
2023-02-27 15:46:06 -0800

    updating 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-  print_array - Prints n elements of an array of integers
+  *print_array - Prints n elements of an array of integers
   * @a: Array of integers
   * @n: Number of elements of the array to be printed
   *
