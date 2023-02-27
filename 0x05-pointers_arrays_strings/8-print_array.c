c7df25991800d38dcde5e9a713900617571e0ed9
engr-Godswill <ochigodswill@gmail.com>
2023-02-27 15:38:37 -0800

    updating 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,16 +2,24 @@
 
 /**
  *print_array-print n-elements of an array 
+ * @a: Array of integers 
  *@n: number of elements in the array
  *return - void
  */
 
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n; i++)
-{
-printf("%d, ", *a[i]);
-}
-_putchar('\n');
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+
+		if (j != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
 }
