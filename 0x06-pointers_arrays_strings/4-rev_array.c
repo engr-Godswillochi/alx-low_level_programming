0754d5fde5fa48bdf8c624e2933ce0afe78c43d0
engr-Godswill <ochigodswill@gmail.com>
2023-03-02 05:42:46 -0800

    updating 4-rev_array to reverse arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * reverse_array - Reverses the content of an array of integers
+  * @a: An array of integers
+  * @n: Number of elements to swap
+  *
+  * Return: empty
+  */
+void reverse_array(int *a, int n)
+{
+  int b;
+  n = b;
+
+  for (; b > 0; b--)
+    {
+      *a[b];
+    }
+}
+
+    
