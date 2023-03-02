2a2983cadd2bdfb84fc699216a93994e60c92e9c
engr-Godswill <ochigodswill@gmail.com>
2023-03-02 04:12:28 -0800

    updating 0-strcat.c to concatenate strings and return dest

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,22 @@
   *
   * Return: A pointer to the resulting string dest
   */
-char *_strcat(char *dest, char *src) {
-    char *p = dest + strlen(dest);
-    while (*src != '\0') {
-        *p++ = *src++;
-    }
-    *p = '\0';
-    return dest;
+
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
 }
