80fc50d27a6b2745cc98986165af423cc3709e33
engr-Godswill <ochigodswill@gmail.com>
2023-03-15 06:14:18 -0700

    updating 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,19 @@
 
 int main(int argc, char *argv[])
 {
-	int a = argv[0], b = argv[1], c = a * b;
+	int n1 = 0, n2 = 0;
 
-	if (argv[] == NULL)
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", c);
-	}
+
+	return (0);
 }
