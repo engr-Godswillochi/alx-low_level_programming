e2d5f973a723ffa27f5b830490e090cb0a50a49d
engr-Godswill <ochigodswill@gmail.com>
2023-03-15 05:35:07 -0700

    1-args to print numbers followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - prins a number followed by a new line
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: main returns zero
+ */
+int main(int argc, char *arg[])
+{
+	(void) argc;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
