8b23b6c23946c71c0d61abfb0b385449f895b966
engr-Godswill <ochigodswill@gmail.com>
2023-03-16 17:26:39 -0700

    updating 0-malloc_checked.c to return a pointer to an allocated memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size to be allocated
+ *
+ * Return - a void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+
+	c = malloc(b);
+
+	if (c == NULL)
+		return (98);
+
+	return (c);
+}
