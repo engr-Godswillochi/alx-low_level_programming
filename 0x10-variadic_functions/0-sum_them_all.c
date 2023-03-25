a5f45072a5994a7caa69517aa9c9855f353a56ea
engr-Godswill <ochigodswill@gmail.com>
2023-03-24 17:51:44 -0700

    updating 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,20 +12,24 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list args;
+	unsigned int i = 0;
+	int sum = 0;
 
+	if (n != 0)
+	{
+		va_start(args, n);
 
-	va_start(ap, n);
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
 
+		va_end(args);
+		return (sum);
+	}
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-
-	va_end(ap);
-
-
-	return (sum);
+	return (0);
 }
 
