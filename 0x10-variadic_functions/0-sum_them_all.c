2fef943a2c6caac98c7221e386b2cff47df073a2
engr-Godswill <122841310+engr-Godswill@users.noreply.github.com>
2023-03-25 02:02:53 +0100

    updated 0-sum_the_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,11 @@
 #include "variadic_functions.h"
 
-
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
- */
+  * sum_them_all - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
@@ -31,4 +28,3 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (0);
 }
-
