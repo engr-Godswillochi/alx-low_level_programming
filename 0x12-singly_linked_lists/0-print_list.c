2177ee3d958d87ea3c5be9b441273ab5a8a6adfc
engr-Godswill <ochigodswill@gmail.com>
2023-03-30 19:11:24 -0700

    fixing some error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,22 +11,17 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
 	size_t count = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str != NULL)
-		{
-			printf("[%d] %s\n", current->len, current->str);
-		}
-		else
-		{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		}
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
+		h = h->next;
 		count++;
-		current = current->next;
 	}
 
 	return (count);
