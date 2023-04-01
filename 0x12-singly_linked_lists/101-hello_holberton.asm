63c682064343df65f06a9f2efd0a77ee155d314f
engr-Godswill <ochigodswill@gmail.com>
2023-03-31 17:56:13 -0700

    updating 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+global _Godswill
+
+section .text
+
+
+_Godswill:
+	MOV rax, 1
+	MOV rdi, 1
+	MOV rsi, msg
+	MOV rdx, msglength
+	SYSCALL
+
+	MOV rax, 60
+	MOV rdi, 0
+	SYSCALL
+
+section .data
+	msg: DB "Hello, Holberton", 10
+	msglength: equ $ - msg
