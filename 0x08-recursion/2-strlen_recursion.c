4e63731f516bf8f4b233847c1b546ac29fb6ef58
engr-Godswill <ochigodswill@gmail.com>
2023-06-08 17:48:12 -0700

    fixing some mistakes

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,14 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
 
 	if (*s == '\0')
 	{
 		return (0);
 	}
-	_strlen_recursion(s);
-	i++;
 	s++;
-	return (i + 1);
+	return (_strlen_recursion(s) + 1);
 }
