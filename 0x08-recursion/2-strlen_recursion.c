d41a6931785b7ca4170c7721bb2489e57faa418a
engr-Godswill <ochigodswill@gmail.com>
2023-06-08 17:36:52 -0700

    a function that reurns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the length of a string
+ * @s: the string to be counted
+ *
+ * Return: th length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	_strlen_recursion(s);
+	i++;
+	s++;
+	return (i);
+}
