22c23314dc4c08a4fb3ace603ccf48d60551f21d
engr-Godswill <ochigodswill@gmail.com>
2023-06-11 16:47:36 -0700

    updating whatsmyname.c

---
+++ 0x0A-argc_arg/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - Prints the sum of args positive numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+
+int main (int argc, char *argv[])
+{
+	(void)_ argc;
+	printf("%s\n, argv[0]");
+
+	return (0);
+}
