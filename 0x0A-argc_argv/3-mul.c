a516de56a9f5446821c8b23421ca7e64fbd85fae
engr-Godswill <ochigodswill@gmail.com>
2023-06-11 18:10:45 -0700

    fixing someberrors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,10 +12,10 @@ int main(int argc, char *argv[])
 {
 	int n1 = 0, n2 = 0;
 
-	n1 = atoi(argv[1]), n2 = atoi(argv[2]);
 
 	if (argc == 3)
 	{
+		n1 = atoi(argv[1]), n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	else
