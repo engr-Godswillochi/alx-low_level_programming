2831d302f47b5eeaa66ac8a9cbb7776c3441a7e6
engr-Godswill <ochigodswill@gmail.com>
2023-06-11 17:52:01 -0700

    a program that multiplies number arguments passed to it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vectort
+ *
+ * Return: always return zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2, result;
+
+	n1 = atoi(argv[1]), n2 = atoi(argv[2]);
+
+	if (argc > 0)
+	{
+		result = n1 * n2;
+		printf("%d", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
