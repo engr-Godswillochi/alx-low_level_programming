6c866012da176e0c3c801bd100dc47ef532d6188
engr-Godswill <ochigodswill@gmail.com>
2023-06-15 17:08:39 -0700

    fixing some betty errors

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,15 +4,15 @@
  * malloc_checked - allocates memory
  * @b: the sixe to be allocated
  *
- * Return: nothing
- */
+ * Return:nothing
+*/
 
 void *malloc_checked(unsigned int b)
 {
 	void *c;
-	
+
 	c = malloc(b);
-	
+
 	if (c == NULL)
 		exit(98);
 
