1d49cf873ebc7f2ab6e44c8f488808b8fbf05cd1
engr-Godswill <ochigodswill@gmail.com>
2023-06-19 08:49:00 -0700

    creating a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct my_dog - ...
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: ...
+ */
+
+struct my_dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
