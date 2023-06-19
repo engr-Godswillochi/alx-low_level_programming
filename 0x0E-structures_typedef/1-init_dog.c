d2fe446c34b68f45646a95dfc041b6caf7735b3f
engr-Godswill <ochigodswill@gmail.com>
2023-06-19 15:58:35 -0700

    fixing some error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog d;
-
-	d.name = "Godswill";
-	d.age = 4.5;
-	d.owner = "Henry";
-
+	if (d != NULL)
+	{
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
+	}
 }
