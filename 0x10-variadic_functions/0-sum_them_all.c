5dbbaaec95f54dd5bd48a82a6b22d18a9f1c6f97
engr-Godswill <ochigodswill@gmail.com>
2023-06-22 18:23:19 -0700

    a function that prints all arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -  function that sums all arguments passed to it
+ * @n: number of arguments
+ *
+ * Return: returns nothing
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list c;
+	int i = 0, x;
+
+	va_start(c, n);
+	for (; i < n; i++)
+	{
+		x = va_arg(c, int);
+		total += x;
+	}
+	va_end(c);
+
+}
